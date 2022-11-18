35e36dbed030c589a1c87ebdd0e56dcf580f04f1
Ismail almahdi <ismailalmahdi.sizmo@gmail.com>
2022-11-18 11:39:03 +0800

    update heroku settings

--- BasicQueueSystem/settings.py
+++ BasicQueueSystem/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/djangoStatic/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #FRONTEND_ROOT = os.path.abspath(os.path.join(BASE_DIR,'..','myfrontend','my-app','build'))
 MEDIA_URL = '/uploads/'
 
